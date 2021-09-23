303f71186d27889a20de03ba224f12f8c4b1f4ce
Altug Cakmakci <altugcakmakci@gmail.com>
2021-09-23 08:40:38 -0400

    timeblocks added

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function init(){
 
 function populateToday() {
     $("#currentDay").text(todayDate.format("MMMM Do, YYYY"));
+    addTImeblocks();
 }
 
 // Jump to previous day
@@ -22,4 +23,36 @@ $('#nextDay').on('click', function () {
     populateToday();
 });
 
+// Add timeblocks
+function addTImeblocks(){
+    let container = $(".container");
+    for(let i = 9;i<18;i++){
+        let thisHour = moment(i,"H");
+        console.log(thisHour);
+        let newBlock = $('<div>');
+        newBlock.addClass("input-group mb-0");
+
+        let newLine = $('<div>');
+        newLine.addClass("input-group-prepend m-0");
+        let newLineSpan = $('<span>');
+        newLineSpan.addClass("input-group-text border-left-0 hourName");
+        newLineSpan.text(thisHour.format("h a"));
+        newLine.append(newLineSpan);
+        newBlock.append(newLine);
+        let newInput = $('<input>');
+        newInput.attr("type","text");
+        newInput.addClass("form-control");
+        newBlock.append(newInput);
+        let newButton = $('<button>');
+        newButton.addClass("btn btn-outline-secondary");
+        newButton.attr("type","button");
+        newButton.text("Check");
+        newBlock.append(newButton);
+
+        container.append(newBlock);
+    }
+}
+
 init();
+
+
