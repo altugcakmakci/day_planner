dc76dd4601fd6006fb028deea2621d51b7d5b91d
Altug Cakmakci <altugcakmakci@gmail.com>
2021-09-23 08:59:52 -0400

    timeblocks colors added

--- script.js
+++ script.js
@@ -25,12 +25,14 @@ $('#nextDay').on('click', function () {
 
 // Add timeblocks
 function addTImeblocks(){
+    let currentHour = 11; //moment().format("H");
+    console.log(currentHour);
     let container = $(".container");
     for(let i = 9;i<18;i++){
         let thisHour = moment(i,"H");
         console.log(thisHour);
         let newBlock = $('<div>');
-        newBlock.addClass("input-group mb-0");
+        newBlock.addClass("input-group mb-0 calanderLine");
 
         let newLine = $('<div>');
         newLine.addClass("input-group-prepend m-0");
@@ -41,10 +43,16 @@ function addTImeblocks(){
         newBlock.append(newLine);
         let newInput = $('<input>');
         newInput.attr("type","text");
-        newInput.addClass("form-control");
+        if (currentHour<i){
+            newInput.addClass("form-control futureHour");
+        } else if (currentHour===i){
+            newInput.addClass("form-control currentHour");
+        } else {
+            newInput.addClass("form-control pastHour");
+        }
         newBlock.append(newInput);
         let newButton = $('<button>');
-        newButton.addClass("btn btn-outline-secondary");
+        newButton.addClass("btn btn-outline-secondary calanderButton");
         newButton.attr("type","button");
         newButton.text("Check");
         newBlock.append(newButton);
